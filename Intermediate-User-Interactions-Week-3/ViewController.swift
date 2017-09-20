a30db4c3eda4a08b9996748cb2bf15f0df48683f
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-09-20 19:31:10 -0400

    Initial Commit

---
+++ Intermediate-User-Interactions-Week-3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intermediate-User-Interactions-Week-3
+//
+//  Created by YASH SOMPURA on 20/09/17.
+//  Copyright © 2017 YASH SOMPURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
