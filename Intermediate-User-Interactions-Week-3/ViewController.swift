c18f577bd7d84a818c201d6a90910643c7b34d1c
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-09-20 20:27:25 -0400

    Added Outlets and Actions

--- Intermediate-User-Interactions-Week-3/ViewController.swift
+++ Intermediate-User-Interactions-Week-3/ViewController.swift
@@ -3,17 +3,46 @@
  * Date: September 20, 2017
  * Student ID: 300967186
  * Description: Intermediate User Interaction Demo
- * Version: 0.1 - Built Basic UI
+ * Version: 0.2 - Added Outlets and Actions
  */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets -------------------------------------------------------------------
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    
+    
+    // Inherited Methods ---------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // Actions Methods -----------------------------------------------------------
 
+    @IBAction func onSliderChange(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func onSegmentedControlChange(_ sender: UISegmentedControl) {
+        
+    }
+    
+    
+    @IBAction func onDoSomethingPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func onSwitchChange(_ sender: UISwitch) {
+    }
+    
 }
 
