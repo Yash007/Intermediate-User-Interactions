50a795bf3e5c8d9ee1d92bea5b273b98be2c4f29
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-09-20 22:06:05 -0400

    Added Partial Functionality

--- Intermediate-User-Interactions-Week-3/ViewController.swift
+++ Intermediate-User-Interactions-Week-3/ViewController.swift
@@ -3,7 +3,7 @@
  * Date: September 20, 2017
  * Student ID: 300967186
  * Description: Intermediate User Interaction Demo
- * Version: 0.2 - Added Outlets and Actions
+ * Version: 0.3 - Added Partial Functionality
  */
 
 import UIKit
@@ -18,7 +18,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sliderLabel: UILabel!
     
+    @IBOutlet weak var leftSwitch: UISwitch!
     
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var doSomethingButton: UIButton!
     
     // Inherited Methods ---------------------------------------------------------
     override func viewDidLoad() {
@@ -27,13 +30,33 @@ class ViewController: UIViewController {
     }
     
     // Actions Methods -----------------------------------------------------------
+    
+    
+    
+    @IBAction func textFieldDoneEditing(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
 
+    @IBAction func onTapGestureRecognizer(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        numberTextField.resignFirstResponder()
+    }
+    
     @IBAction func onSliderChange(_ sender: UISlider) {
-        
+        sliderLabel.text = String(lroundf(sender.value))
     }
     
     @IBAction func onSegmentedControlChange(_ sender: UISegmentedControl) {
-        
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        }
+        else    {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
     }
     
     
@@ -42,6 +65,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onSwitchChange(_ sender: UISwitch) {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting , animated: true)
+        rightSwitch.setOn(setting, animated: true)
+        
+        
     }
     
 }
